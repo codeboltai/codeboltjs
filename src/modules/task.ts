f9e0cb97cc7b3c3713685eb0f52cc7eee83a7bb4
ravi18 <ravi@arrowai.com>
2024-05-01 14:34:01 +0530

    add task added

--- src/modules/task.ts
+++ src/modules/task.ts
@@ -1,20 +1,41 @@
 import cbws from './websocket';
 
 /**
- * Fetches the list of tasks asynchronously.
- * @returns {Promise<any>} A promise that resolves with the list of tasks.
+ * Manages task operations via WebSocket communication.
  */
 const taskplaner = {
+    /**
+     * Adds a task using a WebSocket message.
+     * @param {string} task - The task to be added.
+     * @returns {Promise<any>} A promise that resolves with the response from the add task event.
+     */
+    addTask: async (task: string): Promise<any> => {
+        return new Promise((resolve, reject) => {
+            cbws.getWebsocket.send(JSON.stringify({
+                "type": "addTask",
+                "task": task
+            }));
+            cbws.getWebsocket.on('message', (data: string) => {
+                const response = JSON.parse(data);
+                if (response.type === "addTaskResponse") {
+                    resolve(response); // Resolve the promise with the response data from adding the task
+                }
+            });
+        });
+    },
+    /**
+     * Retrieves all tasks using a WebSocket message.
+     * @returns {Promise<any>} A promise that resolves with the response from the get tasks event.
+     */
     getTasks: async (): Promise<any> => {
         return new Promise((resolve, reject) => {
             cbws.getWebsocket.send(JSON.stringify({
-                "type": "getTasks",
-                
+                "type": "getTasks"
             }));
             cbws.getWebsocket.on('message', (data: string) => {
                 const response = JSON.parse(data);
                 if (response.type === "getTasksResponse") {
-                    resolve(response); // Resolve the Promise with the task data
+                    resolve(response); // Resolve the promise with the response data from retrieving tasks
                 }
             });
         });
