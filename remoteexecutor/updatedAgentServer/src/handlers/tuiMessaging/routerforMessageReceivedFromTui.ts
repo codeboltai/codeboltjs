5e9d174f4ccfd389c9e8a872bb98b10baefd7f3d
Ravi <ravi@arrowai.com>
2025-10-13 16:24:25 +0530

    updated

--- remoteexecutor/updatedAgentServer/src/handlers/tuiMessaging/routerforMessageReceivedFromTui.ts
+++ remoteexecutor/updatedAgentServer/src/handlers/tuiMessaging/routerforMessageReceivedFromTui.ts
@@ -7,10 +7,11 @@ import { SendMessageToAgent } from '../agentMessaging/sendMessageToAgent';
 export class TuiMessageRouter {
   private connectionManager: ConnectionManager;
   private sendMessageToAgent: SendMessageToAgent;
-  
+  private sendMessageToRemote: SendMessageToRemote;
   constructor() {
     this.connectionManager = ConnectionManager.getInstance();
     this.sendMessageToAgent = new SendMessageToAgent();
+    this.sendMessageToRemote = new SendMessageToRemote();
   }
 
   handleTuiMessage(tui: ClientConnection, message: UserMessage | BaseApplicationResponse): void {
@@ -21,8 +22,8 @@ export class TuiMessageRouter {
     }
     else {
       this.sendMessageToAgent.sendResponseToAgent(tui, message as BaseApplicationResponse);
-
     }
+    this.sendMessageToRemote.forwardAppMessage(tui.id, message as BaseApplicationResponse);
   }
 
   handleInitialUserMessage(tui: ClientConnection, message: UserMessage): void {
