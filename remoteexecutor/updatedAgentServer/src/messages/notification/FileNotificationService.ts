07565b81f685f2a69ca026af32548860ddd902cb
Utkarsh Shukla <utk.shukla@gmail.com>
2026-01-14 18:35:38 +0530

    moved

--- remoteexecutor/updatedAgentServer/src/messages/notification/FileNotificationService.ts
+++ remoteexecutor/updatedAgentServer/src/messages/notification/FileNotificationService.ts
@@ -1,6 +1,6 @@
 import type { ClientConnection } from "../../types";
 import type { TargetClient } from "../../shared/utils/ClientResolver";
-import { BaseNotificationService } from "./BaseNotificationService";
+import { BaseNotificationService } from "../../main/notificationManager/BaseNotificationService";
 import type {
   FileReadResponseNotification,
   FileReadErrorNotification,
